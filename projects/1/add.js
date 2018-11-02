4c624589c6c8a9571894607b89f46b4259757583
Artur Wagner <35738771+Eis3@users.noreply.github.com>
2018-11-02 19:36:00 +0000

    UPDATE

---
+++ projects/1/add.js
@@ -0,0 +1,21 @@
+$(".gallery").append('\
+				<div class="post">\
+					<div class="thumbnail">\
+				   		<img src="../projects/1/icon.png">\
+				   	</div>\
+				   	<div class="content">\
+						<div class="header"></div>\
+				   		<div class="content">\
+				   	    	<p class="title">Project methodologies</p>\
+				   	        <p>\
+				   	            &nbsp;&nbsp;&nbsp;&nbsp;This was one of my school projects where I was able to talk\
+								about some project methodologies. The one that I talked about\
+								in this booklet are: Waterfall, Agile and Spiral methodologies<br />\
+								It was a fun experience making this booklet and I hope it helps\
+								someone like you.\
+				   	        </p>\
+				   	    </div>\
+				   	    <div class="footer"><p><a href="../projects/1/project_methodologies.pdf">Check out the project by clicking this link!</a></p></div>\
+				   	</div>\
+				</div>\
+')
