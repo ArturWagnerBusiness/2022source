1e72c2a01ebc87fd7d5914b4fabb15b4cb09a2de
Artur Wagner <35738771+Eis3@users.noreply.github.com>
2018-11-09 22:57:57 +0000

    Added seconds perfectly

--- resources/js/sidebar-date.js
+++ resources/js/sidebar-date.js
@@ -1,7 +1,7 @@
 function addDate(){
     const date = new Date();
-    $("#container #side-col .time .hour").css("transform", "rotate(" + (((date.getHours()/12)*360) + (((date.getMinutes()/60)*360)/12)) + "deg)");
-    $("#container #side-col .time .minute").css("transform", "rotate(" + (date.getMinutes()/60*360) + "deg)");
+    $("#container #side-col .time .hour").css("transform", "rotate(" + (((date.getHours()/12)*360) + (((date.getMinutes()/60)*360)/12) + (((date.getSeconds()/60)*360)/60/12) + "deg)");
+    $("#container #side-col .time .minute").css("transform", "rotate(" + ((date.getMinutes()/60*360)+(date.getSeconds()/60*360)/60) + "deg)");
     $("#container #side-col .time .second").css("transform", "rotate(" + (date.getSeconds()/60*360) + "deg)");
     $("#container #side-col .date").html(date.getDate() + "/" + (date.getMonth()+1/*Month shown 1 less error!*/) + "/" + date.getFullYear());
 }
