cbbd898dd61e3a89371c3561829e5a9b6524e91b
Artur Wagner <35738771+Eis3@users.noreply.github.com>
2018-11-09 22:54:02 +0000

    fix

--- resources/js/sidebar-date.js
+++ resources/js/sidebar-date.js
@@ -1,6 +1,6 @@
 function addDate(){
     const date = new Date();
-    $("#container #side-col .time .hour").css("transform", "rotate(" + (date.getHours()/12*360) + ((date.getMinutes()/60*360)/12) + "deg)");
+    $("#container #side-col .time .hour").css("transform", "rotate(" + (((date.getHours()/12)*360) + (((date.getMinutes()/60)*360)/12)) + "deg)");
     $("#container #side-col .time .minute").css("transform", "rotate(" + (date.getMinutes()/60*360) + "deg)");
     $("#container #side-col .time .second").css("transform", "rotate(" + (date.getSeconds()/60*360) + "deg)");
     $("#container #side-col .date").html(date.getDate() + "/" + (date.getMonth()+1/*Month shown 1 less error!*/) + "/" + date.getFullYear());
